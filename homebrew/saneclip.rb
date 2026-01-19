c7d01b9e7912a5af058c9ef3e886f7426e714eee
SJ <stephanjoseph2007@gmail.com>
2026-01-19 17:21:31 -0500

    Release v1.1: Updated appcast, homebrew SHA256

--- homebrew/saneclip.rb
+++ homebrew/saneclip.rb
@@ -1,6 +1,6 @@
 cask "saneclip" do
   version "1.1"
-  sha256 "35dac44556e600cbf7bcdb418ab78484cf948fef51f16609388aed34a346fe95"
+  sha256 "c86f4c9aa3a2fecdf4e69973846a651a583a27e5ab0ae14401b2e43bd371a5b5"
 
   url "https://github.com/stephanjoseph/SaneClip/releases/download/v#{version}/SaneClip-#{version}.dmg"
   name "SaneClip"
